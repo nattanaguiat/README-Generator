c1cd482fe0eed25201734796d739b4738a5eec39
Nattan Aguia <nattan23@msn.com>
2024-11-02 16:43:58 -0400

    remove develop folder, adds to index,js, updates to writeToFile function

---
+++ index.js
@@ -0,0 +1,84 @@
+// TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown.js';
+import path from 'path';
+
+// TODO: Create an array of questions for user input
+const questions = [
+  { type: 'input',
+    name: 'title',
+    message: 'Please name your proyect',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please describe the purpose and fucntionality of your project',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Please select a license for the project',
+    choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Please describe the installation process for the project',
+  },
+
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Please describe the usage of the project',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Write your GitHub username',
+  },
+
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Write your email address',
+  },
+  {
+    type: 'input',
+    name: 'contributions',
+    message: 'Describe the contribution guidelines for the project',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'List any tests for the project',
+  }
+];
+
+// TODO: Create a function to write README file
+const writeToFile = (fileName, data) => {
+    return fs.writeFile((fileName), data, (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+    })
+}
+
+// TODO: Create a function to initialize app
+const init = () => {
+    //PROMPT - QUESTIONS - CALLBACK => - WRITE TO FILE => (FILE NAME, DATA (GENERATE MARKDOWN =>)) -
+    inquirer.prompt(questions)
+    .then((answers) => {
+    const markDown = generateMarkdown(answers)
+    .then(() => {
+        console.log("Succesfully creating professional README.md")
+    })
+    writeToFile("README.md", answers)
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,48 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+const renderLicenseBadge = (license) => {
+  // LIST OF LICENSES FROM QUESTION (LIST OPTION OF (HOW TO ADD LICENSE))
+  if (license === 'MIT'){
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+  if (license === 'Apache'){
+    return '[![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  }
+  if (license === 'GNU General Publik License (GPL)'){
+    return '[![License: GPLv3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  }
+  if (license === 'Berkley Source Distribution (BSD)'){'[![License: BSD](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+    return 
+  } 
+  if (license === 'None'){
+    return ''
+  }
+
+
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+const renderLicenseLink = (license) => {
+  // CONDITION (IF LICENSE IS ---> RETURN LINK)
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+const renderLicenseSection = (license) => {
+  // SECTION FOR IT ----> HEADER IF THER IS A ONE
+}
+
+// TODO: Create a function to generate markdown for README
+const generateMarkdown = (data) => { // EVERYTHING FOR EVERY SECTION 
+  return `# ${data.title}
+  
+  ## 
+  ## License
+  ##3`;
+}
+
+export default generateMarkdown;
+
+
+
